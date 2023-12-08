589048cf382050a630146aa1734dc15e0d4abe61
Naza <uzoukwuc@tcd.ie>
2023-12-08 16:56:18 +0000

    Before I realised that memoization is not da wae

--- 08/2.pl
+++ 08/2.pl
@@ -1,4 +1,5 @@
 @directions = split //, <> =~ s/[^RL]//r;
+$n = scalar @directions;
 <>; # skip separating line
 /(...) = \((...), (...)\)/, $edge{$1} = { L => $2, R => $3 } while (<>);
 @at = grep { /A$/ } keys %edge;
@@ -11,38 +12,54 @@ advance: {
     }
     
     my $initial = $at[$chosen], $next = $at[$chosen], $delta = 0;
-    
-    print "Advancing ghost $initial with distance $s[$chosen]\n";
-    
-    if (defined $cache{$initial}) {
+    my $key = $initial."-".($s[$chosen] % $n);
+    print "Advancing ghost #$chosen $key with distance $s[$chosen]\n";
+    if (defined $cache{$key}) {
         # If this path has been memoised just use the cache
-        ($next, $delta) = $cache{$initial}->@*;
+        ($next, $delta) = $cache{$key}->@*;
+        $s[$chosen] += $delta;
+        $at[$chosen] = $next;
         print "\tUsing memoised delta where $initial → $next with delta $delta\n"
     } else {
         # Otherwise we need to compute this hoe
         print "\tNeed to compute delta for $initial\n";
         while (1) {
-            $direction = $directions[($s[$chosen]+$delta) % scalar @directions];
-            $next = $edge{$next}{$direction};
+            $next = $edge{$next}{$directions[$s[$chosen] % $n]};
             exit unless defined $next;
-            $delta++;
+            $delta++, $s[$chosen]++;
             if ($next =~ /Z$/) {
                 # memoize here.
-                print "\nMemoizing path $initial → $at with cost $delta\n";
-                $cache{$initial} = [$next, $delta];
+                print "\nMemoizing path $initial → $next with cost $delta\n";
+                $cache{$key} = [$next, $delta];
+                $at[$chosen] = $next;
                 last;
             }
         }
     }
+
+    print "Dumping cache state\n";
+    for (sort { $cache{$a}[1] <=> $cache{$b}[1] } keys %cache) {
+        next unless /Z-/;
+        print "$_ → $cache{$_}[0] ($cache{$_}[1])\n";
+    }
+
+    print "---\n";
+
+    print "Dumping cache state\n";
+    for (sort { $cache{$a}[1] <=> $cache{$b}[1] } keys %cache) {
+        next unless /A-/;
+        print "$_ → $cache{$_}[0] ($cache{$_}[1])\n";
+    }
+
+    # mincut max flow ?
+    # shortest path?
     
-    $s[$chosen] += $delta;
-    $at[$chosen] = $next;
-    
-    # terminating condition is when all nodes are at the same z.  at
+    # terminating condition is when all nodes are at the same z. at
     # each step, we advance the node with the smallest distance.
     for (@s) {
-        redo advance if $_ != $s[0];
+        if ($_ != $s[0]) {
+            print "\t\t$_ does not match $s[0]!\n";
+            redo advance;
+        }
     }
 }
-
-print "$initial reaches $at in $s[0] steps\n";
