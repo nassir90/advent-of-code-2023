3c06e299b145186ed034302d58a30f9126688564
Naza <uzoukwuc@tcd.ie>
2023-12-15 19:05:56 +0000

    Comment

--- 14/2-optimal.pl
+++ 14/2-optimal.pl
@@ -37,7 +37,7 @@ for $block (keys %blocks) {
     my ($row, $column) = ($blockrow, $blockcolumn);
     while ($row -= $dr, $column -= $dc, not $blocks{$row, $column}) {
       last unless 0 <= $row < $width and 0 <= $column < $height;
-      $magnet{$row, $column}{$direction} = [$blockrow, $blockcolumn];
+      $magnet{$direction}{$row, $column} = [$blockrow, $blockcolumn];
     }
   }
 }
@@ -61,10 +61,10 @@ sub engage {
     my ($row, $column) = split $;;
     for (1..$have{$row, $column}) {
       my ($rockrow, $rockcolumn) = ($row - $pdr * $_, $column - $pdc * $_);
-      my ($nextrow, $nextcolumn) = @{$magnet{$rockrow, $rockcolumn}{$direction}};
+      my ($nextrow, $nextcolumn) = @{$magnet{$direction}{$rockrow, $rockcolumn}};
       # print "Based on ($row $column), previous=$previous ($pdr $pdc), next=$direction\n";
       # print "\tMoving rock ($rockrow $rockcolumn) $direction → ($nextrow $nextcolumn)\n";
-      my $delta = ++$next{$nextrow, $nextcolumn};
+      $next{$nextrow, $nextcolumn}++;
     }
   }
   
@@ -72,51 +72,54 @@ sub engage {
 }
 
 sub signature {
-  my %have = @_;
-  join " ",
-    map { join(",", split $;) . ":" . $have{$_} }
-    sort { $a cmp $b } keys %have
+  my ($have) = @_;
+  join $;,
+    sort { $a cmp $b }
+    map { $_ . $; . %$have{$_} }
+    keys %$have
 }
 
+# Takes the data encoded in a %have hash passed by reference and 
 sub formathave {
-  my %have = %{$_[0]};
+  my ($have) = @_;
   my ($dr, $dc) = @_[1..2];
-  
   my %virtual;
-  for (keys %have) {
-    my ($row, $column) = split $;;
-    for (1..$have{$row, $column}) {
-      my ($rockrow, $rockcolumn) = ($row - $dr * $_, $column - $dc * $_);
-      $virtual{$rockrow, $rockcolumn} = "O";
+  for (keys %$have) {
+    my ($blockrow, $blockcolumn) = split $;;
+    for (1..$$have{$blockrow, $blockcolumn}) {
+      $virtual{$blockrow - $dr * $_, $blockcolumn - $dc * $_} = "O";
     }
   }
-    
   my @virtual;
   my @have;
-    
   for $row (0..$width-1) {
     for $column (0..$height-1) {
-      $virtual[$row] .= $virtual{$row, $column} ? "O" : $blocks{$row, $column} ? "#" : ".";
-      $have[$row] .= $blocks{$row, $column} ? 0 + $have{$row, $column} : ".";
+      $virtual[$row] .= $virtual{$row, $column}
+        ? "O" :
+        $blocks{$row, $column} ? "#" : ".";
+      $have[$row] .= $blocks{$row, $column}
+        ? 0 + $$have{$row, $column}
+        : ".";
     }
   }
-  
   return \(@virtual, @have);
 }
 
 sub score {
   my ($have, $dr, $dc) = @_;
   my %have = $have->%*;
-  
   my $score;
   for (keys %have) {
     my ($blockrow, $blockcolumn) = split $;;
     for (1..$have{$blockrow, $blockcolumn}) {
+      # The logic for computing scores is different to what you might
+      # expect here, as we augment the map. The real block row is
+      # obtained by subtracting the top row of introduced '#'
+      # symbols. The real height is the current height minus 2.
       my $rockrow = $blockrow - $dr * $_ - 1;
       $score += $height - 2 - $rockrow;
     }
   }
-  
   return $score;
 }
 
@@ -126,16 +129,16 @@ my $cycles = 10_000;
 # file.
 for (keys %rocks) {
   my ($row, $column) = split $;;
-  my ($nextrow, $nextcolumn) = @{$magnet{$row, $column}{north}};
+  my ($nextrow, $nextcolumn) = @{$magnet{north}{$row, $column}};
   $have{$nextrow, $nextcolumn}++;
 }
 print "Completed first move north\n";
 %have = engage $_ for (west, south, east);
 
 # my ($virtual, $have) = formathave \%have, @east;
-
 # print "\nBelow: $scores[$cycle]\n";
 # print "@$virtual[$_]|@$have[$_]\n" for (0..$height-1);
+my %signature = signature \%have;
 $scores[$cycle] = score \%have, @east;
 $digests[$cycle] = md5_base64($signature);
 $seen{$signature} = 1;
@@ -144,17 +147,17 @@ $cycle++;
 while ($cycle < $cycles) {
   %have = engage $_ for (north, west, south, east);
   
-  my ($virtual, $have) = formathave \%have, @east;
-  my $signature = signature %have;
+  # my ($virtual, $have) = formathave \%have, @east;
   # print "\nBelow: $scores[$cycle]\n";
   # print "@$virtual[$_]|@$have[$_]\n" for (0..$height-1);
+  my $signature = signature \%have;
   $scores[$cycle] = score \%have, @east;
   $digests[$cycle] = md5_base64($signature);
 
   if ($seen{$signature}) {
-    $parallel = $seen{$signature};
+    $cyclestart = $seen{$signature};
     print "it repeats thanks be to god (at cycle $cycle)\n";
-    print "\t$parallel <=> $cycle\n";
+    print "\t$cyclestart <=> $cycle\n";
     last;
   }
   
@@ -166,12 +169,17 @@ while ($cycle < $cycles) {
 print "All scores until repeating section\n";
 printf "\t%3d. $scores[$_] ($digests[$_])\n", $_ for (0..$#scores);
 
-if ($target < $parallel) {
+if ($target < @cycle) {
+  # in this case we didn't go beyond the loop, so we can just index
+  # into the scores array.
   printf "Is the answer: %d?\n", $scores[$target];
 } else {
-  my $offsettarget = $target - $parallel - 1;
-  my @cyclicalscores = @scores[$parallel..$#scores-1];
+  # in this case, we went beyond the cycle. the final scores index is
+  # the cycle starting position, plus the number of cycles after that
+  # modulo the cylce period.
+  my $offsettarget = $target - $cyclestart - 1;
+  my @cycle = @scores[$cyclestart..$#scores-1];
   print "Repeating section\n";
-  printf "\t%3d. $cyclicalscores[$_] ($digests[$parallel+$_])\n", $_ for (0..$#cyclicalscores);
-  printf "Is the answer: %d? ($target %% %d)\n", $cyclicalscores[$offsettarget % @cyclicalscores], scalar @cyclicalscores;
+  printf "\t%3d. $cycle[$_] ($digests[$cyclestart+$_])\n", $_ for (0..$#cycle);
+  printf "Is the answer: %d? ($target %% %d)\n", $cycle[$offsettarget % @cycle], scalar @cycle;
 }
