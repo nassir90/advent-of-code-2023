ea3cbf25e9aff53bfff0e0d300ef8d15fe68ca94
Naza <uzoukwuc@tcd.ie>
2023-12-18 04:06:49 +0000

    Pre dedup

---
+++ 17/fork.pl
@@ -0,0 +1,177 @@
+use warnings;
+
+# each vertex is either visited or unvisted
+# for each visited vertex we store
+# - the heat loss so far
+# - the steps since the last turn
+# our minimum implementation by
+# - heat loss so far
+# and excludes
+# - vertices whose stesps since last turn >= 3
+
+# we will have a distTo hash
+# we will have a vertexTo hash also
+# we will have a stretchTo hash (length of the straight)
+# delete the minimum
+# - filter by last turn < 3
+# - pick vertex with minimum heat loss
+# - advance that hoe
+
+# write perl priority queue afterwards
+
+our $interval = $ENV{INTERVAL} // 200;
+
+our @north = (-1, 0);
+our @west  = (0, -1);
+our @south = (1, 0);
+our @east  = (0, 1);
+
+our @lines = map { chomp; $_ } <>;
+our $width = length $lines[0];
+our $height = @lines;
+
+my $targetrow = $height - 1;
+my $targetcolumn = $width - 1;
+
+our %dist;
+$dist{0, 0} = 0;
+$stretchTo{0, 0} = 0;
+
+our @queue = ([0,0]);
+our %stretchTo;
+our %directionTo;
+our %vertexTo;
+
+our %colors = (red => [255,000,000],
+             orange => [125, 50, 50],
+             yellow => [150, 150, 0 ]);
+
+sub paint {
+  my ($color, $string, $start, $size) = @_;
+  return $string unless $start < length($string);
+  $size = $size // 1;
+  local $" = ";", $color = "@{$colors{$color}}";
+  substr $string, $start, $size, "\e[48;2;${color}m" . substr($string, $start, $size) . "\e[m";
+  return $string;
+}
+
+sub realtruck {
+  my %painted;
+  my @lines = @lines;
+  @_ = sort {@$b[2] <=> @$a[2]} @_;
+  for (@_) {
+    my ($color, $row, $column) = @$_;
+    next if $painted{$row, $column};
+    $lines[$row] = paint $color, $lines[$row], $column, 1;
+    # print "Painting $row, $column $color\n";
+    $painted{$row, $column} = 1;
+  }
+  print "$_\n" for @lines;
+}
+
+
+
+sub dumptruck {
+  my @marks;
+  push @marks, [orange, @$_] for (@queue);
+  push @marks, [yellow, split $;] for (keys %dist);
+  realtruck @marks;
+}
+
+sub esotericdumptruck {
+  my ($row, $column) = @_;
+  my @marks = ([red, 0, 0], [yellow, $row, $column]);
+  while (not $row == 0 && $column == 0) {
+    push @marks, [orange, $row, $column];
+    ($row, $column) = @{$vertexTo{$row, $column}};
+  }
+  realtruck @marks;
+}
+
+search: while (1) {
+  my $minimumrow;
+  my $minimumcolumn;
+  my $minimumdist = Inf;
+  my $chosen = 0;
+  for (0..$#queue) {
+    my ($row, $column) = @{$queue[$_]};
+    if ($dist{$row, $column} < $minimumdist) {
+      $minimumrow = $row;
+      $minimumcolumn = $column;
+      $minimumdist = $dist{$row, $column};
+      $chosen = $_;
+    }
+  }
+  die unless defined $minimumrow;
+  print "Chosen ($minimumrow $minimumcolumn) as starting off pount with distance $minimumdist\n";
+  splice @queue, $chosen, 1, ();
+  unless ($m % $interval) {
+    esotericdumptruck $minimumrow, $minimumcolumn;
+    dumptruck;
+  }
+  if ($minimumrow == $targetrow and $minimumcolumn == $targetcolumn) {
+    my $dist = $dist{$minimumrow, $minimumcolumn};
+    print "Done: $dist\n";
+    last search;
+  }
+  my $previousdirection = $directionTo{$minimumrow, $minimumcolumn};
+  my ($sourcerow, $sourcecolumn) = @{$vertexTo{$minimumrow, $minimumcolumn}};
+  if ($minimumcolumn != 0 and $minimumrow != 0) {
+    die "sourcerow not defined" unless defined $sourcerow;
+    die "sourcecolumn not defined" unless defined $sourcecolumn
+  }
+
+  my @neighbors;
+  my @directions;
+  
+  print("Generating neighbors for ($minimumrow $minimumcolumn)\n");
+  for my $direction (north, south, east, west) {
+    print("\t$direction out of bounds from ($minimumrow $minimumcolumn) [0 → $height]\n"), next
+      unless 0 <= (my $row = $minimumrow + @$direction[0]) < $height;
+    print("\t$direction out of bounds from ($minimumrow $minimumcolumn) [0 → $width]\n"), next
+      unless 0 <= (my $column = $minimumcolumn + @$direction[1]) < $width;
+    print("\tcannot go back as ($row $column) == ($sourcerow $sourcecolumn) from ($minimumcolumn $minimumrow)\n"), next
+      if defined($sourcerow) && ($row == $sourcerow and $column == $sourcecolumn);
+    my $stretch = $stretchTo{$minimumrow, $minimumcolumn};
+    if ($direction eq $previousdirection  && 3 <= 1 + $stretchTo{$minimumrow, $minimumcolumn}) {
+      print("\tCANNOT go straight for longer than three units ($previousdirection → $direction) ($stretch)\n");
+      next;
+    }
+    print("\tIn this case ($previousdirection → $direction → $row, $column) (stretch=$stretch)\n");
+    
+    push @neighbors, [$row, $column];
+    push @directions, $direction;
+  }
+  
+ idekanymore: {
+    my ($destinationrow, $destinationcolumn, $direction) = (@{shift @neighbors}, shift @directions);
+    unless (defined $direction) {
+      die "all should be defined at the same time" if defined $destinationrow;
+      last;
+    };
+    my $cost = substr $lines[$destinationrow], $destinationcolumn, 1;
+    my $dist = $minimumdist + $cost;
+    my $existingdist = $dist{$destinationrow, $destinationcolumn} // Inf;
+    print "From $previousdirection → ( $minimumrow $minimumcolumn) $direction → ($destinationrow, $destinationcolumn) ";
+    print "[cost=$cost, resultingdistance=$dist, existingdistance=$existingdist]\n";
+    if ($dist < $existingdist) {
+      $dist{$destinationrow, $destinationcolumn} = $dist;
+      $vertexTo{$destinationrow, $destinationcolumn} = [$minimumrow, $minimumcolumn];
+      die "bro" if !defined($direction) && (!defined($sourcerow) || $sourcerow != 0) && (!defined($sourcecolumn) || $sourcecolumn != 0);
+      $stretchTo{$destinationrow, $destinationcolumn} = $direction eq $directionTo{$minimumrow, $minimumcolumn}
+        ? 1 +  $stretchTo{$minimumrow, $minimumcolumn}
+        : 0;
+      $directionTo{$destinationrow, $destinationcolumn} = $direction;
+      push @queue, [$destinationrow, $destinationcolumn];
+    }
+    redo;
+  }
+  if ($m++ > ($ENV{HITS} // 200)) {
+    print "💀\n";
+    last;
+  }
+}
+
+esotericdumptruck $targetrow, $targetcolumn;
+
+# new strategy: enumerate all paths with a preference for those closer to 
---
+++ 17/whatsthemata.pl
@@ -0,0 +1,194 @@
+use Data::Dumper;
+local $Data::Dumper::Purity = 1;
+local $Data::Dumper::Deepcopy = 1;
+
+sub paint {
+  my %colors = (red => [255,000,000], orange => [125, 50, 50], yellow => [150, 150, 0 ]);
+  my ($color, $string, $start, $size) = @_;
+  return $string unless $start < length($string);
+  $size = $size // 1;
+  local $" = ";", $color = "@{$colors{$color}}";
+  substr $string, $start, $size, "\e[48;2;${color}m" . substr($string, $start, $size) . "\e[m";
+  return $string;
+}
+
+sub realtruck {
+  my %painted;
+  my @lines = @lines;
+  @_ = sort {@$b[2] <=> @$a[2]} @_;
+  for (@_) {
+    my ($color, $row, $column) = @$_;
+    next if $painted{$row, $column};
+    $lines[$row] = paint $color, $lines[$row], $column, 1;
+    # print "Painting $row, $column $color\n";
+    $painted{$row, $column} = 1;
+  }
+  print "$_\n" for @lines;
+}
+
+# okay, so the rule where you are guaranteed to be fine replacing the
+# path to one place with another holds when both paths are going in
+# the same direction and have the same stretch value.
+
+@lines = map { chomp; $_ } <>;
+$width = length($lines[0]);
+$height = @lines;
+
+# what is in a path?
+# - a hash mapping positions on the path to a [stretch, distance, direction] array.
+
+sub collapse {
+  my ($row, $column, @direction) = @_;
+  my @candidates;
+  my %delete;
+  
+  for (0..$#paths) {
+    my ($otherstretch, $otherdistance, @otherdirection) = @{$paths[$_]{$row, $column}};
+    if ($paths[$_]{$row, $column} and "@direction" eq "@otherdirection") {
+      # print "other path is in sync at ($row $column)\n";
+      push @candidates, $_;
+      $delete{$_} = 1;
+    }
+  }
+  
+  my $s = sub { $paths[$_[0]]->{$row, $column}[0] };
+  my $d = sub { $paths[$_[0]]->{$row, $column}[1] };
+  
+  # sort by distance and try to eliminate by stretch
+  @candidates = sort { $d->($a) <=> $d->($b) } @candidates;
+  my $minimum = 0;
+  my @next;
+  for (0..$#candidates) {
+    $minimum = $candidates[$_] if $s->($candidates[$_]) < $s->($minimum);
+    next unless
+      $_ == $#candidates ||   # final
+      $d->($candidates[$_]) != $d->($candidates[$_+1]); # middle
+    push @next, $minimum;
+  }
+
+  # sort by stretch and try to eliminate by distance
+  @candidates = sort { $s->($a) <=> $s->($b) } @next;
+  my $minimum = 0;
+  my @next;
+  for (0..$#candidates) {
+    $minimum = $candidates[$_] if $d->($candidates[$_]) < $d->($minimum);
+    next unless
+      $_ == $#candidates ||   # final
+      $s->($candidates[$_]) != $s->($candidates[$_+1]); # middle
+    delete $delete{$minimum};
+    push @next, $minimum;
+  }
+
+  splice @paths, $_, 1, () for (sort { $b <=> $a } keys %delete);
+
+  return @paths;
+}
+
+# test filtering by distance
+{
+  local @paths;
+  @north = (-1, 0), @west = (0, -1), @south = (1, 0), @east = (0, 1);
+  $paths[0]->{0, 1} = [1, 10, @east];
+  $paths[1]->{0, 1} = [1, 30, @east];
+  @paths = collapse 0, 1, @east;
+  for (@paths) {
+    print "\t@{$$_{0,1}}\n";
+  }
+}
+
+# test filtering by distance
+{
+  local @paths;
+  @north = (-1, 0), @west = (0, -1), @south = (1, 0), @east = (0, 1);
+  $paths[0]->{0, 1} = [1, 10, @east];
+  $paths[1]->{0, 1} = [1, 30, @east];
+  $paths[2]->{0, 1} = [2, 30, @east];
+  @paths = collapse 0, 1, @east;
+  print "This should be the same as the above\n";
+  for (@paths) {
+    print "\t@{$$_{0,1}}\n";
+  }
+}
+
+# test filtering by distance
+{
+  local @paths;
+  @north = (-1, 0), @west = (0, -1), @south = (1, 0), @east = (0, 1);
+  $paths[0]->{0, 1} = [1, 10, @east];
+  $paths[1]->{0, 1} = [2, 10, @east];
+  $paths[2]->{0, 1} = [1, 30, @east];
+  $paths[3]->{0, 1} = [2, 30, @east];
+  $paths[4]->{0, 1} = [3, 30, @east];
+  @paths = collapse 0, 1, @east;
+  print "This should be the same as the above\n";
+  for (@paths) {
+    print "\t@{$$_{0,1}}\n";
+  }
+}
+
+@paths = ({ tail => [0,0] });
+
+while (1)  {
+  my @children;
+  my %interesting;
+  for my $path (@paths) {
+    my ($row, $column) = @{$$path{tail}};
+    if ($column == $width - 1 && $row == $height - 1) {
+      print "Not continuing finished path\n";
+      next;
+    }
+    my ($stretch, $distance, @direction) = @{$$path{$row, $column}};
+    die unless $row == 0 && $column == 0 || @direction;
+    for my $nextdirection (east, south, west, north) {
+      my ($dr, $dc) = @$nextdirection;
+      my ($nextrow, $nextcolumn) = ($row + $dr, $column + $dc);
+      unless (0 <= $nextrow < $height and 0 <= $nextcolumn < $width) {
+        # print ("$nextdirection ($nextrow $nextcolumn) is OOB\n");
+        next;
+      }
+      if ($nextrow == 0 && $nextcolumn == 0 or exists $$path{$nextrow, $nextcolumn}) {
+        # print ("$nextdirection ($nextrow $nextcolumn) is on this path already\n");
+        next;
+      }
+      my $nextstretch = "@direction" eq "@$nextdirection" ? 1 + $stretch : 1;
+
+      if ($nextstretch >= 3) {
+        # print "Cannot stretch longer 😩\n";
+        next;
+      }
+      
+      my $nextdistance = $distance + substr $lines[$nextrow], $nextcolumn, 1;
+      my %newpath;
+      $newpath{tail} = [$nextrow, $nextcolumn, @$nextdirection];
+      $newpath{$$_[0], $$_[1]} = $$path{$$_[0], $$_[1]} for @{$$path{steps}};
+      $newpath{steps} = [@{$$path{steps}}, [$nextrow, $nextcolumn]];
+      $newpath{$nextrow, $nextcolumn} = [$nextstretch, $nextdistance, @$nextdirection];
+      # print Dumper($newpath);
+      push @children, { %newpath };
+      $interesting{$nextrow, $nextcolumn, "@$nextdirection"} = 1;
+    }
+  }
+  for (keys %interesting) {
+    my ($row, $column, $direction) = split $;;
+    @direction = split $", $direction;
+    @paths = collapse $row, $column, @direction;
+  }
+  push @paths, @children;
+  printf "At $m with %d paths\n", scalar @paths;
+  last if $m++ > 100;
+}
+
+my @lastpaths = grep { $$_{$height-1, $width-1} } @paths;
+my @lastdistances = map { $$_{$height-1, $width-1}[1] } @lastpaths;
+
+print Dumper(@paths);
+printf "Paths created are: %d\n", scalar @paths;
+
+$DB::single = 2;
+
+$x ++;
+
+# to a certain point, there can be an optimal path. that is the one
+# where stretch and didstance are minimised. However, stretch cant be
+# minimised globally, only with resepct to the direction the path is
+# going in.
